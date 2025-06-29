96d07f6925bca81ec6c78ab76f2cab102a28e9d5
Gabriel Huseas <gabescode@protonmail.com>
2025-06-29 13:51:11 -0600

    Add files via upload

---
+++ TargetDummy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TargetDummy : MonoBehaviour, IDamageable
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        Debug.Log($"{gameObject.name} took {amount} damage. Remaining: {health}");
+
+        if (health <= 0)
+        {
+            Debug.Log($"{gameObject.name} died.");
+            Destroy(gameObject);
+        }
+    }
+}
